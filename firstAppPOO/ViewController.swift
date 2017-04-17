27ab42c8e14c7d2ae71fb32164beb9d4dc41ab33
Roman Beltran <morga@imac-de-rafael.local>
2017-04-17 11:36:19 -0600

    Initial Commit

---
+++ firstAppPOO/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  firstAppPOO
+//
+//  Created by Roman Morga Beltran on 17/04/17.
+//  Copyright © 2017 Roman Morga Beltran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
